609b24e917794e316542ee7a149aea3d6dcf4901
GNUDimarik <dmitry.adjiev@gmail.com>
2024-11-20 20:25:06 +0600

    Initial commit

---
+++ allocator.cpp
@@ -0,0 +1,267 @@
+#include "allocator.h"
+#include <cerrno>
+#include <cstring>
+
+namespace utils
+{
+namespace alloc_malloc
+{
+
+static void __try_merge_free_blocks()
+{
+    ALOGD("trying to merge blocks ...");
+
+    if (detail::_S_heap_start != nullptr && detail::_S_heap_end != nullptr)
+    {
+        for (_MemoryBlock_t __cur_blk = _MemoryBlock_t::__from_user_space_memory(detail::_S_heap_start);
+                __cur_blk.__header() < detail::_S_heap_end;
+                __cur_blk = __cur_blk.__next_implicit_block())
+        {
+            __cur_blk.__try_coalesc_with_next_implicit_block();
+        }
+    }
+}
+
+int memory_init(uint8_t *__start, size_t __size_in_bytes) noexcept
+{
+    if (__start != nullptr && __size_in_bytes > 0)
+    {
+        // first and last blocks must be always allocated
+        size_t __reserved_block_size = _MemoryBlock_t::_S_header_size << 1;
+        size_t __total_reserved_space = __reserved_block_size << 1;
+        detail::_S_heap_start = __start;
+        detail::_S_heap_end = detail::_S_heap_start + __size_in_bytes - __reserved_block_size;
+
+        // Set to heap end pointer to usable space of the last block
+        // Mark it as allocated block with size __SIZEOF_POINTER__
+        _MemoryBlock_t heapStart(detail::_S_heap_start);
+        heapStart.__put_to_header(_MemoryBlock_t::_S_header_size, _MemoryBlock_t::_BlockState::_S_allocated);
+
+        _MemoryBlock_t heap = heapStart.__next_implicit_block();
+        // Take in account overhead for heap memory block it self so formula is
+        // total heap size  - reserved space - heap memory block overhead size
+        heap.__put_to_header(__size_in_bytes - __total_reserved_space - _MemoryBlock_t::_S_header_size,
+                             _MemoryBlock_t::_BlockState::_S_free);
+
+        // Create the last allocated service block
+        _MemoryBlock_t heapEnd = heap.__next_implicit_block();
+        heapEnd.__put_to_header(_MemoryBlock_t::_S_header_size, _MemoryBlock_t::_BlockState::_S_allocated);
+        return 0;
+    }
+
+    return EINVAL;
+}
+
+/**
+ * @brief __mem_find_fist_fit
+ * @param __size
+ * @return
+ */
+_MemoryBlock_t __mem_find_fist_fit(size_t __size)
+{
+    if (detail::_S_heap_start != nullptr && detail::_S_heap_end != nullptr)
+    {
+        for (_MemoryBlock_t __cur_blk = detail::_S_heap_start; __cur_blk.__header() <= detail::_S_heap_end;
+                __cur_blk = __cur_blk.__next_implicit_block())
+        {
+            if (__cur_blk.__is_free() && __cur_blk.__size() >= __size)
+            {
+                return __cur_blk;
+            }
+        }
+    }
+
+    return _MemoryBlock_t::__null_block();
+}
+
+/**
+ * @brief mem_malloc - allocates unused space for an object whose size in bytes is
+ *                 specified by size and whose value is unspecified.
+ * @param size   - desired size of block in bytes
+ * @return         Upon successful completion with size not equal to 0, mem_malloc()
+ *                 shall return a pointer to the allocated space. If size is 0,
+ *                 either:
+ *
+ * A null pointer shall be returned and errno set to 0
+ * Otherwise, it shall return a null pointer and set errno to ENOMEM
+ */
+void* mem_malloc(size_t __size) noexcept
+{
+    if (__size > 0)
+    {
+        auto block = __mem_find_fist_fit(_MemoryBlock_t::__aligned_size(__size));
+
+        if (block.__is_null())
+        {
+            // No block. Try to merge some free blocks if possible and search again
+            __try_merge_free_blocks();
+            block = __mem_find_fist_fit(__size);
+        }
+
+        if (!block.__is_null())
+        {
+            return block.__slice(__size).__user_space_memory();
+        }
+
+        ALOGE("Out of memory");
+    }
+
+    return nullptr;
+}
+
+/**
+ * @brief mem_calloc Allocates memory for an array of num objects of size and initializes all bytes in the allocated storage to zero.
+ * @param num number of objects
+ * @param size 	size of each object
+ * @return On success, returns the pointer to the beginning of newly allocated memory
+ */
+void* mem_calloc(size_t __num, size_t __size) noexcept
+{
+    size_t __sz = __size * __num;
+    void *p = mem_malloc(__sz);
+
+    if (p != nullptr)
+    {
+        __sz = _MemoryBlock_t::__aligned_size(__sz);
+        memset(p, 0, __sz);
+    }
+
+
+    return p;
+}
+
+/**
+ * @brief mem_realloc Reallocates the given area of memory. If ptr is not nullptr it must be previously
+ *                    allocated by mem_malloc, mem_calloc or mem_realloc.
+ *                    If __p is nullptr, the behavior is the same as calling mem_malloc(__new_sz).
+ * @param __p pointer to the memory area to be reallocated
+ * @param __new_sz new size of the array in bytes
+ * @return On success, returns the pointer to the beginning of newly allocated memory
+ */
+void* mem_realloc(void *__p, size_t __new_sz) noexcept
+{
+    if (__p == nullptr)
+    {
+        ALOGD("Block '%p' is null. Allocating memory with size(%lu)...", __p, __new_sz);
+        return mem_malloc(__new_sz);
+    }
+
+    _MemoryBlock_t __blk = _MemoryBlock_t::__from_user_space_memory(__p);
+
+    if (__blk.__is_allocated())
+    {
+        if (__new_sz > 0)
+        {
+            if (__blk.__size() == __new_sz)
+            {
+                ALOGD("new block size exactly as needed!");
+                return __p;
+            }
+
+            if (__new_sz > __blk.__size())
+            {
+                ALOGD("new size is above than current. Trying to coleasc");
+                __blk.__try_coalesc_with_next_implicit_block();
+            }
+
+            if (__blk.__size() > __new_sz)
+            {
+                ALOGD("new block size more than needed, slicing ...");
+                return __blk.__slice(__new_sz).__user_space_memory();
+            }
+
+            // Here new block needs to be allocated and previous block needs to be copied to
+            void* __new_blk = mem_malloc(__new_sz);
+
+            if (__new_blk != nullptr)
+            {
+                ALOGD("Trying to allocate new block and copy contents of old one there");
+                memcpy(__new_blk, __blk.__user_space_memory(), __blk.__size());
+                __blk.__unallocate();
+                __blk.__try_coalesc_with_next_implicit_block();
+                return __new_blk;
+            }
+
+            ALOGD("No memory for new block. mem_realloc failed!");
+        }
+    }
+    else
+    {
+        // if block is not allocated do nothing, just return nullptr. actually it's UB
+        ALOGE("Block '%p' is not allocated!", __p);
+    }
+
+    return nullptr;
+}
+
+/**
+ * @brief mem_free free previously allocated memory
+ * @param ptr pointer to user space address of block
+ */
+void mem_free(void* __p) noexcept
+{
+    // we should be able savely called with nullptr
+    if (__p != nullptr)
+    {
+        auto block = _MemoryBlock_t::__from_user_space_memory(__p);
+
+        if (!block.__is_null() && block.__is_allocated()) {
+            block.__unallocate();
+            block.__try_coalesc_with_next_implicit_block();
+        }
+    } else {
+        ALOGE("Unknown block '%p'", __p);
+    }
+}
+
+/**
+ * @brief __dump __dump memory
+ */
+
+void __dump() noexcept
+{
+    size_t __total_memory = 0;
+    size_t __total_free_memory = 0;
+    size_t __total_allocated_memory = 0;
+    size_t __total_with_overhead = 0;
+    size_t __total_blocks = 0;
+
+    ALOGD(
+        "*************************MEMORY DUMP*************************");
+
+    if (detail::_S_heap_start != nullptr && detail::_S_heap_end != nullptr)
+    {
+        for (_MemoryBlock_t __cur_blk = detail::_S_heap_start; __cur_blk.__header() <= detail::_S_heap_end;
+                __cur_blk = __cur_blk.__next_implicit_block(), __total_blocks++)
+        {
+            __cur_blk.__dump();
+            __total_memory += __cur_blk.__size();
+            __total_with_overhead += __cur_blk.__size_with_overhead();
+
+            if (__cur_blk.__is_allocated())
+            {
+                __total_allocated_memory += __cur_blk.__size();
+            }
+            else if (__cur_blk.__is_free())
+            {
+                __total_free_memory += __cur_blk.__size();
+            }
+        }
+    }
+    else
+    {
+        ALOGD("Not initialized");
+    }
+
+    ALOGD(
+        "**********************END OF MEMORY DUMP**********************");
+
+    ALOGD("total memory               %12lu bytes", __total_memory);
+    ALOGD("total memory with overhead %12lu bytes", __total_with_overhead);
+    ALOGD("total allocated memory     %12lu bytes", __total_allocated_memory);
+    ALOGD("total free memory          %12lu bytes", __total_free_memory);
+    ALOGD("total total_blocks count   %12lu", __total_blocks);
+}
+
+} // namespace alloc_malloc
+} // namespace utils
---
+++ allocator.h
@@ -0,0 +1,301 @@
+#ifndef ALLOCATOR_H
+#define ALLOCATOR_H
+
+#include <cstdint>
+#include <stddef.h>
+
+#define LOG_NDEBUG 1
+#define LOG_TAG "_MemoryBlock"
+#include "logging.h"
+
+namespace utils
+{
+namespace alloc_malloc
+{
+namespace detail
+{
+
+static uint8_t* _S_heap_start = nullptr;
+static uint8_t* _S_heap_end = nullptr;
+
+template <std::size_t _PointerSize> struct _MemoryBlock
+{
+
+    /**
+     * @brief __aligned_size
+     * @param __size
+     * @return aligned by _PointerSize size
+     */
+    static size_t __aligned_size(size_t __size) noexcept
+    {
+        if (__size < _S_header_size)
+        {
+            return _S_header_size;
+        }
+
+        __size  = ((__size + (_S_header_size - 1)) & ~(_S_header_size - 1));
+        __size += _S_header_size;
+        return __size;
+    }
+
+    /**
+     * @brief __null_block
+     * @return nullptr block
+     */
+    static _MemoryBlock __null_block() noexcept
+    {
+        return _MemoryBlock(nullptr);
+    }
+
+    /**
+     * @brief _S_size_of_pointer actually word should be address size but in terms of
+     * asm it's even qword.
+     */
+    static constexpr const int _S_size_of_pointer = _PointerSize;
+
+    /**
+     * @brief _S_header_size size of implcit header
+     */
+    static constexpr const int _S_header_size = _S_size_of_pointer;
+
+    /**
+     * @brief The _BlockState enum means block state allocated or free
+     */
+    enum class _BlockState : int
+    {
+        _S_free = 0,       //!< Block is not allocated i.e. free
+        _S_allocated = 1,  //!< Block is allocated
+    };
+
+    /**
+     * @brief _M_block pointer to memory block which starts from implicit header
+     */
+    uint8_t* _M_block = nullptr;
+
+    /**
+     * @brief _MemoryBlock creates _MemoryBlock from __addr. It's header address not user space
+     * @param __addr start of block
+     */
+    _MemoryBlock(uint8_t* __addr) : _M_block(__addr) {}
+
+    /**
+     * @brief __from_user_space_memory
+     * @param __address - block address which starts from first byte of usable
+     * space. i.e. after implicit header
+     * @return
+     */
+    inline static _MemoryBlock __from_user_space_memory(void* __address)
+    {
+        return _MemoryBlock(reinterpret_cast<uint8_t*> (__address) - _S_header_size);
+    }
+
+    /**
+     * @brief header
+     * @return pointer to implcit header of the block
+     */
+    inline uint8_t* __header() const noexcept
+    {
+        return _M_block;
+    }
+
+    /**
+     * @brief __user_space_memory
+     * @return usable space of block. i.e. space which may be used by user
+     * clients
+     */
+    inline uint8_t* __user_space_memory() const noexcept
+    {
+        return __header() + _S_header_size;
+    }
+
+    /**
+     * @brief size
+     * @return size of block
+     */
+    inline size_t __size() const noexcept
+    {
+        return (*reinterpret_cast<size_t*>(__header()) & ~0x01);
+    }
+
+    /**
+     * @brief __size_with_overhead
+     * @return total size of block including implicit header and footer
+     */
+    inline size_t __size_with_overhead() const noexcept
+    {
+        return __size() + _S_header_size;
+    }
+
+    /**
+     * @brief __is_allocated
+     * @return true if block is allocated
+     */
+    inline bool __is_allocated() const noexcept
+    {
+        return (*reinterpret_cast<size_t*>(__header()) & 0x01) ==
+               static_cast<int>(_BlockState::_S_allocated);
+    }
+
+    /**
+     * @brief __is_free
+     * @return true if block is free
+     */
+    bool __is_free() const noexcept
+    {
+        return !__is_allocated();
+    }
+
+    /**
+     * @brief __put_to_header set data to implicit header packed to one word
+     * @param size size of block
+     * @param state block state
+     */
+    void __put_to_header(size_t __size, _BlockState __state = _BlockState::_S_free) noexcept
+    {
+        *reinterpret_cast<size_t*>(__header()) = __size | static_cast<int>(__state);
+    }
+
+    /**
+     * @brief __next_implicit_block
+     * @return next implicit block
+     */
+    _MemoryBlock __next_implicit_block() const noexcept
+    {
+        return _MemoryBlock(__header() + __size() + _S_header_size);
+    }
+
+    /**
+     * @brief previousImplicitBlock
+     * @return previous implicit block
+     */
+    _MemoryBlock previousImplicitBlock() const noexcept
+    {
+        return _MemoryBlock(__header() - __size_with_overhead());
+    }
+
+    /**
+     * @brief __slice tries to split this block to two if current block has enough space
+     * @param __sz requested size of second block
+     * @return next block after the split with size __sz and current block has size before this operation - __sz
+     * (values may be corrected depend from block overheads)
+     * slicing happens from end of the block.
+     * if this block had no enough space for that null block is returned @see __is_null
+     */
+    _MemoryBlock __slice(size_t __sz) noexcept
+    {
+        if (__sz > 0)
+        {
+            __sz = __aligned_size(__sz);
+
+            if (__sz < __size())
+            {
+                size_t __new_sz = __size() - (__sz + _S_header_size);
+#if SLICE_FROM_BEINNING
+                __put_to_header(__size, _BlockState::_S_allocated);
+                __next_implicit_block().__put_to_header(__new_sz, _BlockState::_S_free);
+                return *this;
+#else
+                __put_to_header(__new_sz, _BlockState::_S_free);
+                __next_implicit_block().__put_to_header(__sz, _BlockState::_S_allocated);
+                return __next_implicit_block();
+#endif
+            }
+            else if (__sz == __size())
+            {
+                __put_to_header(__sz, _BlockState::_S_allocated);
+                return *this;
+            }
+            else
+            {
+                ALOGD(
+                    "%s(): current block size (%lu) less than requested (%lu). Ignore "
+                    "request\n",
+                    __func__, __size(), __sz);
+            }
+        }
+
+        return __null_block();
+    }
+
+    /**
+     * @brief __try_coalesc_with_next_implicit_block tries to merge as much as possible contiguous blocks
+     * @return true if at least once merge operation been succeeded (this block and it's next)
+     */
+    bool __try_coalesc_with_next_implicit_block() noexcept
+    {
+        if (__is_free())
+        {
+            auto __next = __next_implicit_block();
+
+            if (!__next.__is_null() && __next.__is_free())
+            {
+                __put_to_header(__size() + __next.__size() + _S_header_size);
+                __next = __next_implicit_block();
+
+                while (__next.__try_coalesc_with_next_implicit_block())
+                {
+                    __put_to_header(__size() + __next.__size() + _S_header_size);
+                    __next = __next_implicit_block();
+                }
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @brief __unallocate set free state to implicit header of the block
+     */
+    inline void __unallocate() noexcept
+    {
+        __put_to_header(__size(), _BlockState::_S_free);
+    }
+
+    /**
+     * @brief __is_null
+     * @return true if block points to nullptr
+     */
+    bool __is_null() const noexcept
+    {
+        return _M_block == nullptr;
+    }
+
+    /**
+     * @brief operator < should help us to compare blocks during search of first or best fit
+     * @param rhs - another block to compare
+     * @return true if this < rhs by size
+     */
+    bool operator < (const _MemoryBlock<_PointerSize> &__rhs) const noexcept
+    {
+        return __size() < __rhs.__size();
+    }
+
+    void __dump() const noexcept
+    {
+        const char* format = _M_block == _S_heap_start || _M_block == _S_heap_end
+                             ? "service block address %p size %12lu \t size "
+                             "with overhead %8lu state %s"
+                             : "block address         %p size %12lu \t size "
+                             "with overhead %8lu state %s";
+
+        ALOGD(format, __user_space_memory(), __size(), __size_with_overhead(),
+              __is_allocated() ? "allocated" : "free");
+    }
+};
+
+}  // namespace details
+
+using _MemoryBlock_t = detail::_MemoryBlock<__SIZEOF_POINTER__>;
+
+int memory_init(uint8_t *__start, size_t __size_in_bytes) noexcept;
+void* mem_malloc(size_t __size) noexcept;
+void* mem_calloc(size_t __num, size_t __size) noexcept;
+void* mem_realloc(void *__p, size_t __new_sz) noexcept;
+void mem_free(void* __p) noexcept;
+void __dump() noexcept;
+}  // namespace alloc_malloc
+} // namespace utils
+
+#endif  // ALLOCATOR_H
