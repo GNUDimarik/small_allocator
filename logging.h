26d0109cb178477d89a7db4bad7a5b3a1e633406
GNUDimarik <dmitry.adjiev@gmail.com>
2024-11-21 01:10:54 +0600

    Added forgotten logging.h

---
+++ logging.h
@@ -0,0 +1,27 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
+#if LOG_NDEBUG
+#   ifdef __linux__
+#       include <stdio.h>
+#           define ALOGD(__ARGS__...)    \
+            {                           \
+                fprintf(stdout, "%s:\t", LOG_TAG); \
+                fprintf(stdout, __ARGS__);         \
+                fprintf(stdout, "\n");             \
+                fflush(stdout); \
+            }
+#       define ALOGE(__ARGS__...)             \
+            {                                    \
+                fprintf(stderr, "%s:\t", LOG_TAG); \
+                fprintf(stderr, __ARGS__);         \
+                fprintf(stderr, "\n");             \
+                fflush(stdout); \
+        }
+#   endif /* __linux__ */
+#else
+#   define ALOGD(...)
+#   define ALOGE(...)
+#endif /* LOG_NDEBUG */
+
+#endif // DEBUG_H
