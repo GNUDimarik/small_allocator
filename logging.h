f644a71b83d5a0ae31bf361e9f20aa010fa23d60
Dmitry Adzhiev <dmitry.adjiev@gmail.com>
2026-06-15 23:43:01 +0300

    Disable logs in freestanding mode for now

--- logging.h
+++ logging.h
@@ -26,26 +26,28 @@
 #define LOGGING_H
 
 #if !LOG_NDEBUG
-#   ifndef __ANDROID__
-#       include <stdio.h>
-#           define ALOGD(__ARGS__...)    \
-            {                           \
-                fprintf(stdout, "%s:\t", LOG_TAG); \
-                fprintf(stdout, __ARGS__);         \
-                fprintf(stdout, "\n");             \
-                fflush(stdout); \
+#   if !(defined __OSDEV_FREESTANDING)
+#       ifndef __ANDROID__
+#           include <stdio.h>
+#               define ALOGD(__ARGS__...)    \
+                {                           \
+                    fprintf(stdout, "%s:\t", LOG_TAG); \
+                    fprintf(stdout, __ARGS__);         \
+                    fprintf(stdout, "\n");             \
+                    fflush(stdout); \
+                }
+#           define ALOGE(__ARGS__...)             \
+                {                                    \
+                    fprintf(stderr, "ERROR: %s:\t", LOG_TAG); \
+                    fprintf(stderr, __ARGS__);         \
+                    fprintf(stderr, "\n");             \
+                    fflush(stderr); \
             }
-#       define ALOGE(__ARGS__...)             \
-            {                                    \
-                fprintf(stderr, "ERROR: %s:\t", LOG_TAG); \
-                fprintf(stderr, __ARGS__);         \
-                fprintf(stderr, "\n");             \
-                fflush(stderr); \
-        }
-#   endif /* __linux__ */
+#       endif /* __linux__ */
 #else
-#   define ALOGD(...)
-#   define ALOGE(...)
+#       define ALOGD(...)
+#       define ALOGE(...)
+#   endif /* __FREESTANDING__ */
 #endif /* LOG_NDEBUG */
 
 #endif //LOGGING_H
